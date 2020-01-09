95101746972b6f7b93a0bcd573af221e2320851b
Thomas Koppensteiner <thomas.koppensteiner@space-4.me>
2020-01-09 20:56:28 +0100

    Refactor web service spec

--- spec/geonames/web_service_spec.rb
+++ spec/geonames/web_service_spec.rb
@@ -3,11 +3,25 @@ require 'spec_helper'
 module Geonames
   describe WebService do
     describe ".make_request" do
-      it "uses a custom User-Agent header" do
-        expect(Net::HTTP::Get).to receive(:new).with(anything, hash_including('User-Agent' => USER_AGENT))
+      subject { WebService.make_request '/foo?a=a' }
 
-        Net::HTTP.stub :start
-        WebService.make_request '/foo?a=a'
+      let(:expected_request) do
+        stub_request(:get, "http://api.geonames.org/foo?a=a&lang=en").
+          with(
+            headers: {
+              'User-Agent'=>'geonames ruby webservice client 0.3.5'
+            }
+          ).to_return(status: 200, body: "", headers: {})
+      end
+
+      before do
+        expected_request # stub request
+      end
+
+      it "uses a custom User-Agent header" do
+        subject
+
+        expect(expected_request).to have_been_made
       end
     end
   end
